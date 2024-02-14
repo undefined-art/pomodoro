cffa310c9d38aa9b45cb45c21f18631a9973f8f1
Monulph <95251720+undefined-art@users.noreply.github.com>
2024-02-14 19:42:05 +0100

    CORE-10: Server CoreExceptionsFilter with interceptors for timeout and response

---
+++ server/src/interceptors/response-interceptor.ts
@@ -0,0 +1,32 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable()
+export class ResponseInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<unknown> {
+    return next
+      .handle()
+      .pipe(map((res: unknown) => this.responseHandler(res, context)));
+  }
+
+  responseHandler(res: any, context: ExecutionContext) {
+    const ctx = context.switchToHttp();
+    const response = ctx.getResponse();
+    const request = ctx.getRequest();
+
+    const statusCode = response.statusCode;
+
+    return {
+      status: statusCode,
+      timestamp: new Date().toISOString(),
+      path: request.url,
+      message: res.message,
+    };
+  }
+}
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,13 +1,26 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as session from 'express-session';
 import * as cookieParser from 'cookie-parser';
 import { ValidationPipe } from '@nestjs/common';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+import { CoreExceptionsFilter } from './global-filters/core-exceptions.filter';
+import { ResponseInterceptor } from './interceptors/response-interceptor';
+import { TimeoutInterceptor } from './interceptors/timeout.interceptor';
 
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, { cors: true });
+  const httpAdapterHost = app.get(HttpAdapterHost);
+
+  app.setGlobalPrefix('api/v1');
   app.use(cookieParser());
+  app.useGlobalFilters(new CoreExceptionsFilter(httpAdapterHost));
+
+  app.useGlobalInterceptors(
+    new TimeoutInterceptor(),
+    new ResponseInterceptor(),
+  );
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
